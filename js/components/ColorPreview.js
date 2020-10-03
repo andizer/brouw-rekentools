0c06653dd8ee8bd20e8ac7c562ec32b2457833b1
Andizer <andy.meerwaldt@gmail.com>
2020-10-03 22:54:33 +0200

    Fixes styling bug

--- js/components/ColorPreview.js
+++ js/components/ColorPreview.js
@@ -41,7 +41,7 @@ const ColorPreviewElement = styled.span`
   width: 25px;
   height: 25px;
   display: inline-block;
-  background: ${ props => props.color || "#050B0A" }
+  background: ${ props => props.color || "#050B0A" };
   border-radius: 100%;
   border: 3px solid #FFF;  
   box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
