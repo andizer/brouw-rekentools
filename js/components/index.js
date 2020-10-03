1081618d67f179f420c71f19ecc09daadcbb4345
Andizer <andy.meerwaldt@gmail.com>
2020-10-03 23:18:21 +0200

    Clean up the components that are almost identical

--- js/components/index.js
+++ js/components/index.js
@@ -1,22 +1,16 @@
 import Button from "./Button";
-import Gravity from "./Gravity";
 import Label from "./Label";
 import NumberField from "./NumberField";
 import StaticField from "./StaticField";
-import Temperature  from "./Temperature";
 import ColorPreview from "./ColorPreview";
-import Volume from "./Volume";
 import Weight, { WeightHelp } from "./Weight";
 
 export {
   Button,
-  Gravity,
   Label,
   NumberField,
   StaticField,
-  Temperature,
   ColorPreview,
-  Volume,
   Weight,
   WeightHelp,
 };
