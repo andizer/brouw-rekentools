827d60f14cfe9c375fe9923a4c8aff803f432b75
Andizer <andy.meerwaldt@gmail.com>
2020-10-03 14:59:09 +0200

    Fix normalization of the gravity

--- js/calculation/normalizeGravity.js
+++ js/calculation/normalizeGravity.js
@@ -6,8 +6,8 @@
  * @returns {number} The normalized value.
  */
 export const normalizeGravity = ( value ) => {
-    if ( ( ( value / 1000 ) - 1 ) < 1 ) {
-        return value / 1000;
+    if ( value < 1 || ( value - 1 ) < 1 ) {
+        return value * 1000;
     }
 
     return value;
