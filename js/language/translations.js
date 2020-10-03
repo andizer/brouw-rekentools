f2620b70066180fc9b967be52f377b1bdebecddf
Andizer <andy.meerwaldt@gmail.com>
2020-10-03 21:31:47 +0200

    Implement and initialize the i18n library

---
+++ js/language/translations.js
@@ -0,0 +1,23 @@
+
+const translations = {
+  en: {
+    original_gravity: 'Original gravity',
+    final_gravity: 'Final gravity',
+    alcohol_percentage: 'Alcohol percentage',
+
+    help: {
+      gravity: 'The gravity must have a value between %{min} and %{max}.',
+    },
+  },
+  nl: {
+    original_gravity: 'Begin SG',
+    final_gravity: 'Eind SG',
+    alcohol_percentage: 'Alcoholpercentage',
+
+    help: {
+      gravity: 'De waarde SG moet liggen tussen de %{min} en %{max}.',
+    },
+  },
+};
+
+export default translations;
--- js/redux/store.js
+++ js/redux/store.js
@@ -1,7 +1,25 @@
 
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { loadTranslations, setLocale, syncTranslationWithStore, i18nReducer } from 'react-redux-i18n';
 import { reducers } from "./reducers";
+import translations from "../language/translations";
 
-const rootReducer = combineReducers( { ...reducers } );
+const rootReducer = combineReducers( {
+  ...reducers,
+  i18n: i18nReducer
+} );
 
-export default createStore( rootReducer );
+const store = createStore( rootReducer, applyMiddleware( thunk ) );
+
+syncTranslationWithStore( store );
+store.dispatch( loadTranslations( translations ) );
+
+let locale = 'en';
+if ( document.brew_calculations ) {
+  locale = document.brew_calculations.locale;
+}
+
+store.dispatch( setLocale( locale ) );
+
+export default store;
