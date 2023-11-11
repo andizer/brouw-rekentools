faf29d307937d6872a92c3718ad73ab47020f579
Andy Meerwaldt <andy.meerwaldt@gmail.com>
2023-11-11 12:40:01 +0100

    Removes unused form composites

--- js/composites/form/index.js
+++ js/composites/form/index.js
@@ -1,13 +1,9 @@
 import FormGroup from './FormGroup';
 import StaticFormGroup from "./StaticFormGroup";
-import Column from './Column';
-import Row from './Row';
 import Help from './Help';
 
 export {
   FormGroup,
   StaticFormGroup,
-  Column,
-  Row,
   Help,
 }
