992e4e5f0a457f5f53b3e1455b1406b4b374bb36
Andy Meerwaldt <andy.meerwaldt@gmail.com>
2023-11-11 22:56:13 +0100

    Moves the form-control class definition to the field self

---
+++ js/language/resources.js
@@ -0,0 +1,20 @@
+export default {
+  en: {
+    translation: {
+      "original_gravity": "Original gravity",
+      "final_gravity": "Final gravity",
+      "alcohol_percentage": "Alcohol percentage",
+      "color": "Color",
+      "gravityHelp": "The gravity must have a value between {{min}} and {{max}}.",
+    }
+  },
+  nl: {
+    translation: {
+      "original_gravity": "Begin SG",
+      "final_gravity": "Eind SG",
+      "alcohol_percentage": "Alcoholpercentage",
+      "color": "Kleur",
+      "gravityHelp": "De waarde SG moet tussen de {{min}} en {{max}} liggen.",
+    }
+  },
+};
